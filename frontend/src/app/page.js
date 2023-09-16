c2807a14a5b9bb656b74b941eb47426e33337f16
ritikZangoh <ritik@zangoh.com>
2023-09-16 12:50:54 +0530

    update

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,16 +2,18 @@
 import React, { useState, useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
 import ClipLoader from 'react-spinners/ClipLoader'
+import { Icon } from '@iconify/react'
 
 export default function Home() {
   const [typeOfContent, setTypeOfContent] = useState('')
   const [message, setMessage] = useState('')
   const [charCount, setCharCount] = useState(0)
   const [responseMessage, setResponseMessage] = useState('')
-  const [responseWordCount, setResponseWordCount] = useState(0);
+  const [responseWordCount, setResponseWordCount] = useState(0)
   const [paidStatus, setPaidStatus] = useState(true) // State for billing status
   const [readOnly, setReadOnly] = useState(false) // State for readonly
   const [loading, setLoading] = useState(false)
+  const [waitForResponse, setWaitForResponce] = useState(false)
 
   const searchParams = useSearchParams()
 
@@ -27,7 +29,7 @@ export default function Home() {
   // To set the readOnly state
   useEffect(() => {
     setReadOnly(searchParams.get('modaltype') === 'read' ? true : false)
-    initializeChatBot();
+    initializeChatBot()
   }, [])
 
   // To change state to unpaid
@@ -53,19 +55,22 @@ export default function Home() {
   // To get billing status of user
   async function initializeChatBot() {
     try {
-      const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/initialize_chatbot',{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_API_URL + '/api/initialize_chatbot',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ modalId: modalID }),
         },
-        body: JSON.stringify({ modalId: modalID }),
-      })
+      )
       const responseData = await response.json()
 
       if (response.status == 200) {
-        let paid = responseData.paid == 0 ? false : true;
+        let paid = responseData.paid == 0 ? false : true
         setPaidStatus(paid)
-        setTypeOfContent(responseData.contentType);
+        setTypeOfContent(responseData.contentType)
       }
     } catch (error) {
       console.error('Error:', error)
@@ -75,17 +80,21 @@ export default function Home() {
 
   // To handle the generate content
   async function handleGenerateContent() {
-    if ( !message ) return
-    setLoading(true);
+    if (!message || waitForResponse) return
+    setWaitForResponce(true)
+    setLoading(true)
 
     try {
-      const responce = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/handle_request', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+      const responce = await fetch(
+        process.env.NEXT_PUBLIC_API_URL + '/api/handle_request',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ modalId: modalID, msg: message }),
         },
-        body: JSON.stringify({ modalId: modalID, msg: message }),
-      })
+      )
 
       const responseData = await responce.json()
 
@@ -94,11 +103,12 @@ export default function Home() {
       } else {
         setResponseMessage(responseData.error)
       }
-      setResponseWordCount(responseData.msg.trim().split(/\s+/).length);
+      setResponseWordCount(responseData.msg.trim().split(/\s+/).length)
     } catch (error) {
       console.log('Error: ' + error)
     }
-    setLoading(false);
+    setLoading(false)
+    setWaitForResponce(false)
   }
 
   return (
@@ -110,10 +120,10 @@ export default function Home() {
         className="w-[491px] shadow-md relative rounded-xl px-6 py-5"
         style={{ width: width + 'px', height: height + 'px' }}
       >
-        <ClipLoader
+        {/* <ClipLoader
           cssOverride={{ position: 'absolute', top: '20px', right: '50%' }}
           loading={loading}
-        />
+        /> */}
         <div className="relative">
           <h2
             className="text-[14px] font-bold mt-6 text-[#16192C]"
@@ -129,7 +139,9 @@ export default function Home() {
             className="w-full bg-[#F2F2F2] mt-[10px] px-[18px] py-3 rounded-md"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            onKeyDown={(e) =>{ if(e.key === 'Enter') handleGenerateContent()}}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') handleGenerateContent()
+            }}
             disabled={!paidStatus}
             type="text"
             required
@@ -146,6 +158,13 @@ export default function Home() {
             }}
           >
             Generate
+            {loading && (
+              <Icon
+                className="h-6 w-10 scale-[2] text-purple-600 ml-2 inline-block"
+                style=""
+                icon="eos-icons:three-dots-loading"
+              />
+            )}
           </button>
 
           <h2
@@ -166,7 +185,9 @@ export default function Home() {
             value={responseMessage}
             onChange={(e) => setResponseMessage(e.target.value)}
           ></textarea>
-          <p className="text-xs text-right text-[#868686] mt-1">Words: {responseWordCount}</p>
+          <p className="text-xs text-right text-[#868686] mt-1">
+            Words: {responseWordCount}
+          </p>
         </div>
       </div>
     </div>
